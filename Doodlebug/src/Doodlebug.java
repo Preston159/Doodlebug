b83fc9b73cabbaafe6096acb5e86d543bef09ac9
Terry <Terry@152.228.117.239>
2018-02-26 12:28:32 -0600

    kk

--- Doodlebug/src/Doodlebug.java
+++ Doodlebug/src/Doodlebug.java
@@ -10,7 +10,28 @@ public class Doodlebug extends Organism {
 	
 	@Override
 	public void move() {
-		
+		int[] direction = Main.getDirection();
+		int nx = this.x + direction[0];
+		int ny = this.y + direction[1];
+		if(nx < 0 || nx > 19 || ny < 0 || ny > 19) {
+			move();
+		}
+		else if(Main.board[this.x + 1][this.y] instanceof Ant){
+			this.x+=1;
+		}
+		else if(Main.board[this.x - 1][this.y] instanceof Ant){
+			this.x-=1;
+		}
+		else if(Main.board[this.x][this.y+1] instanceof Ant){
+			this.y+=1;
+		}
+		else if(Main.board[this.x][this.y-1] instanceof Ant){
+			this.y-=1;
+		}
+		else {
+			this.x+=nx;
+			this.y+=ny;
+		}
 	}
 	
 	@Override
--- Doodlebug/src/Organism.java
+++ Doodlebug/src/Organism.java
@@ -4,5 +4,9 @@ public abstract class Organism {
 	public abstract void move();
 	public abstract Organism breed();
 	
+	@Override
+	public String toString() {
+		return "OR";
+	}
 	
 }
