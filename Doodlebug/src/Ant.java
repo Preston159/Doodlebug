63f9442ef36be7d62e7b2043804057134be75c18
Preston <prestonpetrie@gmail.com>
2018-02-26 10:58:07 -0600

    Add unimplemented methods

--- Doodlebug/src/Ant.java
+++ Doodlebug/src/Ant.java
@@ -1,6 +1,15 @@
 
 public class Ant extends Organism {
-	
-	
+
+	@Override
+	public void move() {
+		
+	}
+
+	@Override
+	public Organism breed() {
+		
+		return null;
+	}
 	
 }
--- Doodlebug/src/Doodlebug.java
+++ Doodlebug/src/Doodlebug.java
@@ -1,6 +1,15 @@
 
 public class Doodlebug extends Organism {
 	
+	@Override
+	public void move() {
+		
+	}
 	
+	@Override
+	public Organism breed() {
+		
+		return null;
+	}
 	
 }
