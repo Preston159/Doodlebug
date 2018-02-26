e482ff027f9674ff1f8604e116f5932921b2a3af
Terry <Terry@152.228.117.239>
2018-02-26 11:00:51 -0600

    added displaybaord and board

--- Doodlebug/src/Main.java
+++ Doodlebug/src/Main.java
@@ -1,12 +1,28 @@
 
 public class Main {
 	
+	static Organism[][] board = new Organism[20][];
+	static {
+		for(int i = 0;i < 20;i++) {
+			board[i] = new Organism[20];
+		}
+	}
+	
 	public static void main(String[] args) {
-		
+		displayBoard();
 	}
 	
 	public static void step() {
 		
+	}
+	
+	public static void displayBoard() {
+		for(Organism[] r : board) {
+			for(Organism c : r) {
+				System.out.print(c);
+			}
+			System.out.println();
+		}
 	}
 	
 }
